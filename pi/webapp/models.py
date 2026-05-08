ac18e08517042cb40d3d51a3020d5a22ad77d229
Re <>
2026-05-07 23:32:37 -0300

    20260507

--- pi/webapp/models.py
+++ pi/webapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Cadastro(models.Model):
+    nome = models.CharField(max_length=100)
+    tel = models.CharField(max_length=20)
+    data_nasc = models.DateField()
+    assinatura = models.ImageField(null=True, blank=True)
+    def __str__(self):
+        return self.nome
